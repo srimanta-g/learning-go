2dace48fdf1bd6436aa0fea2fe4439777f53ce11
srimanta-g <srimantaganguly02@gmail.com>
2025-06-30 19:59:50 +0530

    Learning slices in GO

---
+++ slices/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	array := []int{1, 2, 3, 4, 5}
+
+	array = append(array, 6)
+
+	array2 := make([]int, 0, 10)
+	array2 = append(array2, 1)
+	array2 = append(array2, 2, 3, 4, 5)
+
+	fmt.Println(array2)
+	fmt.Println(len(array2))
+	fmt.Println(cap(array2))
+}
