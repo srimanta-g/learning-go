b6ffec88b38e16eeadf5a715284e99960321afed
srimanta-g <srimantaganguly02@gmail.com>
2025-06-30 14:08:36 +0530

    Learning arrays in GO

---
+++ arrays/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// fmt.Println("Learning ararys in Go!")
+
+	// var arr [5]int64
+
+	// arr[0] = 10
+	// arr[3] = 20
+
+	// strArr := [5]string{"Hello", "World", "Go", "Programming", "Language"}
+	// var secondArray [10]string
+
+	// fmt.Printf("String Array: %q", secondArray)
+
+	// for index, value := range strArr {
+	// 	fmt.Printf("Index: %d, Value: %s\n", index, value)
+	// }
+}
